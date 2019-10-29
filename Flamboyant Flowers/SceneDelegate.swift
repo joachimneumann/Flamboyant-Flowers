c09f3ff452ae673c455ac4376f1f3e5b4027e9d9
Joachim Neumann <joachim@mindo.io>
2019-10-29 12:51:31 +0100

    swiftlint

--- Flamboyant Flowers/SceneDelegate.swift
+++ Flamboyant Flowers/SceneDelegate.swift
@@ -14,25 +14,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var startedFromShortCut = false
 
-    func scene(
-    _ scene: UIScene,
-    willConnectTo
-    session: UISceneSession,
-    options connectionOptions: UIScene.ConnectionOptions) {
-        for userActivityy: NSUserActivity in connectionOptions.userActivities
-        where userActivityy.title == "show a flamboyant flower" {
-                startedFromShortCut = true
-            }
-        }
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+                for userActivityy: NSUserActivity in connectionOptions.userActivities
+                    where userActivityy.title == "show a flamboyant flower" {
+                        startedFromShortCut = true
+                    }
 
-        // Use this method to optionally configure and attach the UIWindow `window` to the
-        // provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be
-        // initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new
-        // (see `application:configurationForConnectingSceneSession` instead).
-        guard _ = (scene as? UIWindowScene) else { return }
-    }
+        // Use this method to optionally configure and attach the
+        // UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically
+        // be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are
+        // new (see `application:configurationForConnectingSceneSession` instead).
+        guard (scene as? UIWindowScene) != nil else { return }
+     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
